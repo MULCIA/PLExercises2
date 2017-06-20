f21112b492baf83733b2bcb933d2f729c2b6ab1e
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-06-20 08:32:41 +0200

    Refactor

--- ex1.pl
+++ ex1.pl
@@ -1,9 +1,19 @@
-vacio([]).
-raiz([_,N,_], N).
-hi([HI,_,_],HI).
-hd([_,_,HD],HD).
-hazarbol(R,HI,HD,[HI,R,HD]).
+/*
 
-escribe_arbol(_,ABN,_) :- vacio(ABN).
-escribe_arbol(R,ABN,L) :- raiz(ABN,R).
-escribe_arbol(R,ABN,L) :- hi(ABN,HI), hd(ABN,HD).
+?- escribe_arbol(6,[[[[[],1,[]],5,[]],7,[[],3,[[],4,[]]]],6,[[[],10,[]],8,[[[],9,[]],11,[[],2,[]]]]],L).
+
+*/
+
+escribe_arbol(N,ABN,L):- explora_hijos(N,ABN,HS), construye_lista(HS,L).
+
+explora_hijos(R,[],[]).
+explora_hijos(R,[HI,R,HD],[HI,R,HD]).
+explora_hijos(R,[HI,_,HD],HS) :-
+    explora_hijos(R,HI,HS),
+    explora_hijos(R,HD,HS).
+
+construye_lista([],[]).
+construye_lista([HI,R,HD],[R|Rs]) :-
+    construye_lista(HI,HIs),
+    construye_lista(HD,HDs),
+    append(HIs,HDs,Rs).
