5fe28c1eb2bd99a861df11ef8bcce695acc2f461
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-06-21 11:19:00 +0200

    Completado, aunque resulve en espejo

--- ex1.pl
+++ ex1.pl
@@ -1,12 +1,12 @@
 /*
 
-?- escribe_arbol([[[[[],1,[]],5,[]],7,[[],3,[[],4,[]]]],6,[[[],10,[]],8,[[[],9,[]],11,[[],2,[]]]]],L,P).
-L = [6, 7, 5, 1, 3, 4, 8, 10, 11|...],
-P = [0, 1, 2, 3, 2, 3, 1, 2, 2|...] .
+?-  escribe_arbol([[[[[],1,[]],5,[]],7,[[],3,[[],4,[]]]],6,[[[],10,[]],8,[[[],9,[]],11,[[],2,[]]]]],NS).
+NS = [6, 8, 7, 11, 10, 3, 5, 2, 9|...] .
 
 */
 
-escribe_arbol(ABN,L,P):- explora_hijos(ABN,L,P,0).%, ordena_lista().
+% Recibe un ABN y una lista de Nodos ordenados vacia. Consigue la lista de nodos por busqueda en profundidad junto a sus niveles, para ser ordenados despues.
+escribe_arbol(ABN,NS) :- explora_hijos(ABN,N,L,0), insert_sort(L,N,LS,NS). %insert_sort(L,N,NS).
 
 explora_hijos([],[],_,_).
 explora_hijos([[],R,[]],[R|Rs],[N|Ps],N).
@@ -16,6 +16,36 @@ explora_hijos([HI,R,HD],[R|Rs],[N|Ps],N) :-
     explora_hijos(HD,HDs,PDs,N2),
     append(HIs,HDs,Rs),
     append(PIs,PDs,Ps).
+
+insert_sort(List,List2,Sorted,Sorted2):-i_sort(List,List2,[],[],Sorted,Sorted2).
+
+i_sort([],[],Acc,AccO,Acc,AccO).
+i_sort([H|T],[HO|TO],Acc,AccO,Sorted,SortedO):-insert(H,HO,Acc,AccO,NAcc,NAccO),i_sort(T,TO,NAcc,NAccO,Sorted,SortedO).
+
+insert(X,XO,[Y|T],[YO|TO],[Y|NT],[YO|NTO]):-X>Y,insert(X,XO,T,TO,NT,NTO).
+insert(X,XO,[Y|T],[YO|TO],[X,Y|T],[XO,YO|TO]):-X=<Y.
+insert(X,XO,[],[],[X],[XO]).
+
+/*insert_sort(List,P,Sorted,S):-i_sort(List,P,[],Sorted,S).
+
+i_sort([],[],Acc,Acc,Acc).
+i_sort([H|T],[P|Ps],Acc,Sorted,S):-insert(H,P,Acc,NAcc,NSAcc),i_sort(T,Ps,NAcc,Sorted,NSAcc).
+
+insert(X,A,[Y|T],[Y|NT],[S|Ss]):-X>Y,insert(X,A,T,NT,Ss).
+insert(X,A,[Y|T],[X,Y|T],S):-X=<Y.
+insert(X,A,[],[X],[A]).*/
+
+
+
+
+/*insert_sort(List,L,Sorted,S):-i_sort(List,L,[],Sorted,S).
+
+i_sort([],[],Acc,Acc,Acc).
+i_sort([H|T],[L|Ls],Acc,Sorted,S):-insert(H,L,Acc,NAcc,NSAcc),i_sort(T,Ls,NAcc,Sorted,NSAcc).
+
+insert(X,A,[Y|T],[Y|NT],[S|Ss]):-X>Y,insert(X,A,T,NT,Ns).
+insert(X,A,[Y|T],[X,Y|T],S):-X=<Y.
+insert(X,A,[],[X],[A]).*/
 
 /*ordena_lista([],[]).
 ordena_lista([HI,R,HD],[R|Rs]) :-
