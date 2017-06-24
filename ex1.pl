69413b7cdabf7267ad7feebc8642110e92e3e730
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-06-24 17:12:13 +0200

    Remove comments

--- ex1.pl
+++ ex1.pl
@@ -25,30 +25,3 @@ i_sort([H|T],[HO|TO],Acc,AccO,Sorted,SortedO):-insert(H,HO,Acc,AccO,NAcc,NAccO),
 insert(X,XO,[Y|T],[YO|TO],[Y|NT],[YO|NTO]):-X>Y,insert(X,XO,T,TO,NT,NTO).
 insert(X,XO,[Y|T],[YO|TO],[X,Y|T],[XO,YO|TO]):-X=<Y.
 insert(X,XO,[],[],[X],[XO]).
-
-/*insert_sort(List,P,Sorted,S):-i_sort(List,P,[],Sorted,S).
-
-i_sort([],[],Acc,Acc,Acc).
-i_sort([H|T],[P|Ps],Acc,Sorted,S):-insert(H,P,Acc,NAcc,NSAcc),i_sort(T,Ps,NAcc,Sorted,NSAcc).
-
-insert(X,A,[Y|T],[Y|NT],[S|Ss]):-X>Y,insert(X,A,T,NT,Ss).
-insert(X,A,[Y|T],[X,Y|T],S):-X=<Y.
-insert(X,A,[],[X],[A]).*/
-
-
-
-
-/*insert_sort(List,L,Sorted,S):-i_sort(List,L,[],Sorted,S).
-
-i_sort([],[],Acc,Acc,Acc).
-i_sort([H|T],[L|Ls],Acc,Sorted,S):-insert(H,L,Acc,NAcc,NSAcc),i_sort(T,Ls,NAcc,Sorted,NSAcc).
-
-insert(X,A,[Y|T],[Y|NT],[S|Ss]):-X>Y,insert(X,A,T,NT,Ns).
-insert(X,A,[Y|T],[X,Y|T],S):-X=<Y.
-insert(X,A,[],[X],[A]).*/
-
-/*ordena_lista([],[]).
-ordena_lista([HI,R,HD],[R|Rs]) :-
-    ordena_lista(HI,HIs),
-    ordena_lista(HD,HDs),
-    append(HIs,HDs,Rs).*/
